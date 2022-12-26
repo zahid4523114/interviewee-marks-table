61155d231db448d04bdae69095607cd436cc021e
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-12-26 14:39:36 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import "./App.css";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/layout";
+import ModalTable from "./components/modal/ModalTable";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App bg-gradient-to-r from-cyan-400 to-blue-400 min-h-screen p-10 ">
+      <ChakraProvider>
+        <Box
+          borderRadius="2xl"
+          padding="6"
+          width="80%"
+          mx="auto"
+          boxShadow="2xl"
+          backgroundColor="white"
         >
-          Learn React
-        </a>
-      </header>
+          <TableContainer>
+            <Table variant="striped" colorScheme="facebook">
+              <TableCaption>Interviewee marks and feedback</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>name</Th>
+                  <Th>Date</Th>
+                  <Th>Time</Th>
+                  <Th>Result</Th>
+                  <Th>Feedback</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>Shahadat Hossain</Td>
+                  <Td>26-12-22</Td>
+                  <Td>30m</Td>
+                  <Td>70</Td>
+                  <Td>
+                    <ModalTable></ModalTable>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Imran Mridha</Td>
+                  <Td>26-12-22</Td>
+                  <Td>30m</Td>
+                  <Td>70</Td>
+                  <Td>
+                    <ModalTable></ModalTable>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Ikbal Mondol</Td>
+                  <Td>26-12-22</Td>
+                  <Td>30m</Td>
+                  <Td>70</Td>
+                  <Td>
+                    <ModalTable></ModalTable>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Md Riaj</Td>
+                  <Td>26-12-22</Td>
+                  <Td>30m</Td>
+                  <Td>70</Td>
+                  <Td>
+                    <ModalTable></ModalTable>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Zahid Hasnan</Td>
+                  <Td>26-12-22</Td>
+                  <Td>30m</Td>
+                  <Td>70</Td>
+                  <Td>
+                    <ModalTable></ModalTable>
+                  </Td>
+                </Tr>
+              </Tbody>
+              <Tfoot></Tfoot>
+            </Table>
+          </TableContainer>
+        </Box>
+      </ChakraProvider>
     </div>
   );
 }
---
+++ src/components/modal/ModalTable.tsx
@@ -0,0 +1,42 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { Button, Text, useDisclosure } from "@chakra-ui/react";
+
+const ModalTable = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button variant="solid" onClick={onOpen}>
+        See feedback
+      </Button>
+
+      <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Feedback</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontWeight="bold" mb="1rem">
+              You can scroll the content behind the modal
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button color="black" variant="ghost" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalTable;
